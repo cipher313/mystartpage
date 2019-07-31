69d7154720c01a7e0716beb00c13ddbcdc010194
cipher313 <51022613+cipher313@users.noreply.github.com>
2019-07-31 21:11:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function getDateTime() {
         hour = 24 + hour;
     }
 
-    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
+    let date = dateTime.getFullYear() + ' / ' + (month < 10 ? '0' + month : month) + ' / ' (day < 10 ? '0' + day : day) ;
     let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 
     return date + '\n' + time;
